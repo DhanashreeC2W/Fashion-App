c20c91632a003ec5d9f1562640ee2d27bffa2815
DhanashreeDD <dhanashreeflutter@gmail.com>
2024-08-27 15:58:06 +0530

    "Completed with Provider"

--- lib/view/checkout_screen.dart
+++ lib/view/checkout_screen.dart
@@ -1,5 +1,4 @@
 import 'package:fashion_app/controller/items_data.dart';
-import 'package:fashion_app/controller/payment_mode_list.dart';
 import 'package:fashion_app/view/widget/payment_details.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
