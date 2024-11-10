b8812c4688b3a667e6c4d6d8fc66868fb3286925
DhanashreeC2W <dhanashreeflutter@gmail.com>
2024-11-10 23:22:27 +0530

    "minor changes done"

--- lib/view/widget/bottom_navigation.dart
+++ lib/view/widget/bottom_navigation.dart
@@ -10,43 +10,53 @@ class MyBottomNavigation extends StatefulWidget {
 class _MyBottomNavigationState extends State<MyBottomNavigation> {
   @override
   Widget build(BuildContext context) {
-    return BottomNavigationBar(
-        // fixedColor: Color.fromRGBO(255, 255, 255, 1),
-        backgroundColor: const Color.fromRGBO(255, 255, 255, 1),
-        selectedItemColor:const Color.fromRGBO(234, 133, 82, 1) ,
-        type: BottomNavigationBarType.fixed,
-        items: const [
-          BottomNavigationBarItem(
-            icon: Icon(
-              Icons.home_outlined,
-            ),
-            label: "Home",
-            backgroundColor: Color.fromRGBO(13, 13, 14, 1),
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(
-              Icons.search_rounded,
-              color: Color.fromRGBO(13, 13, 14, 1),
-            ),
-            label: "Search",
-            backgroundColor: Color.fromRGBO(13, 13, 14, 1),
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(
-              Icons.shopping_bag_outlined,
-              color: Color.fromRGBO(13, 13, 14, 1),
-            ),
-            label: "Cart",
-            backgroundColor: Color.fromRGBO(13, 13, 14, 1),
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(
-              Icons.settings_outlined,
-              color: Color.fromRGBO(13, 13, 14, 1),
-            ),
-            label: "Settings",
-            //backgroundColor: Color.fromRGBO(13,13,14,1),
-          ),
-        ]);
+    return Column(
+      children: [
+        Container(
+          
+                height: 3,
+                color: const Color.fromRGBO(255, 122, 0, 1), // Orange color line
+              
+          child: BottomNavigationBar(
+              // fixedColor: Color.fromRGBO(255, 255, 255, 1),
+              backgroundColor: const Color.fromRGBO(255, 255, 255, 1),
+              selectedItemColor:const Color.fromRGBO(234, 133, 82, 1) ,
+              type: BottomNavigationBarType.fixed,
+              items: const [
+                BottomNavigationBarItem(
+                  icon: Icon(
+                    Icons.home_outlined,
+                  ),
+                  label: "Home",
+                  backgroundColor: Color.fromRGBO(13, 13, 14, 1),
+                ),
+                BottomNavigationBarItem(
+                  icon: Icon(
+                    Icons.search_rounded,
+                    color: Color.fromRGBO(13, 13, 14, 1),
+                  ),
+                  label: "Search",
+                  backgroundColor: Color.fromRGBO(13, 13, 14, 1),
+                ),
+                BottomNavigationBarItem(
+                  icon: Icon(
+                    Icons.shopping_bag_outlined,
+                    color: Color.fromRGBO(13, 13, 14, 1),
+                  ),
+                  label: "Cart",
+                  backgroundColor: Color.fromRGBO(13, 13, 14, 1),
+                ),
+                BottomNavigationBarItem(
+                  icon: Icon(
+                    Icons.settings_outlined,
+                    color: Color.fromRGBO(13, 13, 14, 1),
+                  ),
+                  label: "Settings",
+                  //backgroundColor: Color.fromRGBO(13,13,14,1),
+                ),
+              ]),
+        ),
+      ],
+    );
   }
 }
